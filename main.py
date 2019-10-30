23370f77b79ba5248dce51fc3627d4f3f1dd52b0
Samuel Cardonis <o528060707@gmail.com>
2019-10-30 10:49:36 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,168 @@
+# Copyright 2016 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#gcloud projects create dbkeyvalue
+#gcloud config set project dbkeyvalue
+#gcloud app deploy
+
+
+import webapp2
+from google.appengine.ext import ndb
+#from google.cloud import ndb
+#from google.cloud import datastore
+#import google.cloud.exceptions
+import random
+
+
+class Score(ndb.Model):
+    score = ndb.IntegerProperty()
+    timestamp = ndb.DateTimeProperty(auto_now_add=True)
+
+
+class Storage():
+    def score_key(self):
+        return ndb.Key('Score', 'Store')
+
+    def populate(self):
+        new_score = Score(parent=self.score_key())
+        new_score.score = random.randint(1, 1234)
+        new_score.put()
+
+    def get_score(self):
+        score_query = Score.query(ancestor=self.score_key()).order(-Score.timestamp)
+        return score_query.get().score
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        print ('gettt')
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello db key value simon !')
+        self.response.write(' gcloud app deploy succeeded !')
+
+class GetHandler(webapp2.RequestHandler):
+    def get(self):
+        print ('gettt')
+        variable_name = self.request.get('name')
+        print ('variable_name='+variable_name)
+        k = ndb.Key(urlsafe=variable_name)
+        v = k.get()
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('the value of the variable variable_name '+variable_name+' is '+v+'.')
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('get !')
+
+    def get_key(client):
+        # [START datastore_named_key]
+        #client = datastore.Client()#"project_id")
+        key = client.key('Task', 'sample_task')
+        # [END datastore_named_key]
+
+        return key
+
+class SetHandler(webapp2.RequestHandler):
+    def get(self):
+
+        print ('/set?name=n&value=v')
+        variable_name  = self.request.get('name')
+        variable_value = self.request.get('value')
+
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('/set?name=n&value=v : '+variable_name +"="+variable_value)
+        self.response.headers['Content-Type'] = 'text/plain'
+        #self.response.write('set !')
+
+
+class UnsetHandler(webapp2.RequestHandler):
+    def get(self):
+        print ('/unset')
+        variable_name = self.request.get('name')
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('/unset?name='+variable_name)
+
+
+class NumEqualToHandler(webapp2.RequestHandler):
+    def get(self):
+        print ('/numequalto')
+        variable_value = self.request.get('value')
+        # variable_value = 0
+        # query = client.query(kind='Task')
+        # query.add_filter('start', '=', variable_value)
+
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('/numequalto  variable_value='+variable_value)
+
+
+class UndoHandler(webapp2.RequestHandler):
+    def get(self):
+        print ('UndoHandler')
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('UndoHandler !')
+
+
+class RedoHandler(webapp2.RequestHandler):
+    def get(self):
+        print ('RedoHandler')
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('RedoHandler !')
+
+
+class EndHandler(webapp2.RequestHandler):
+    def get(self):
+        print ('EndHandler')
+        kind_list = ['Task']
+
+        #namespace_manager.set_namespace(namespace) # will set to the namespace provided
+
+        for a_kind in kind_list:
+            # will fetch keys of all objects in that kind
+            kind_keys = a_kind.gql("").fetch(keys_only = True)
+            # will delete all the keys at once
+            ndb.delete_multi(kind_keys)
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('EndHandler !')
+
+
+
+        # app = webapp2.WSGIApplication([
+#     ('/', MainPage),
+# ], debug=True)
+
+# class NewHandler(webapp2.RequestHandler):
+#     def get(self):
+#         client = datastore.Client('dbkeyvalue')
+#         print ('NewHandler')
+#         task = datastore.Entity(client.key('Tasks'))
+#         task.update({
+#             'category': 'Personal',
+#             'done': False,
+#             'priority': 4,
+#             'description': 'Learn Cloud Datastore'
+#         })
+#
+#         self.response.headers['Content-Type'] = 'text/plain'
+#         self.response.write('New Entity created !')
+
+
+app = webapp2.WSGIApplication([
+    ('/'     ,	MainHandler),
+    ('/get'  ,	GetHandler),
+    ('/set'  , 	SetHandler),
+    ('/unset',  UnsetHandler),
+    ('/numequalto', NumEqualToHandler),
+    ('/undo' , 	 UndoHandler),
+    ('/redo' , 	 RedoHandler),
+    ('/end'  ,   EndHandler),
+    #('/new'  ,   NewHandler),
+     ], debug=False)
+
+
